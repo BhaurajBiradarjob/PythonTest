00420365c6a2bea9665beae8afaa5ff20b14972c
BhaurajBiradarjob <156108104+BhaurajBiradarjob@users.noreply.github.com>
2024-05-06 20:01:12 +0530

    class 9

--- DataTypes/Dictionary.py
+++ DataTypes/Dictionary.py
@@ -84,10 +84,90 @@ d={key1:value1, key2:value2,....., keyN:valueN}
 # print(d) # error
 
 # dict() - This function can be used to create an empty dictionary
-d=dict()
-print(d)
-print(type(d))
+# d=dict()
+# print(d)
+# print(type(d))
 
-d1=dict({1:"Python", 2:"C++"})
-print(d1)
-print(type(d1))
+# d1=dict({1:"Python", 2:"C++"})
+# print(d1)
+# print(type(d1))
+
+# dict([tuple1, tuple2])
+# This creates a dictionary with list of tuple elements
+# dp = dict({1:"Python", 2:"C++"})
+# print(dp, type(dp))
+
+# len()
+# d1=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+# print(f"Length of dictionary : {len(d1)}")
+
+# get() - is used to get the value associated with the key, if key is 
+# available then it returns the correspounding value otherwise return None
+# d=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+# result=d.get(2)
+# print(result)
+# result=d.get(3)
+# print(result)
+
+# result=d.get(5)
+# print(result)
+# result=d.get(7, "No key found")
+# print(result)
+
+#pop() - Method removes element and expects one argument
+# d1=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+# print(d1)
+# result = d1.pop(3) 
+# print(result)
+# print(d1)
+# print(d1.pop(3)) # Error - already removed item
+
+#popitem() - removes an arbitrary item(key-value) from dictionary and returns it
+# Gives an error when dictionary is empty
+# d1=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+# result = d1.popitem()
+# print(result)
+# print(d1)
+
+# keys() - returns all keys associated with dictionary
+# d1=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+# print(d1)
+
+# for x in d1.keys():
+#     print(x)
+
+# values() - Returns all values associated with dictionary
+# d1=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+# print(d1)
+
+# for x in d1.values():
+#     print(x)
+
+# items() - each key value pair is called item, item() method returns the list
+# of tuples representing key-value pair
+# d1=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+# print(d1)
+
+# for k,v in d1.items():
+#     print(f"The key is {k} and Value is {v}")
+
+#copy()
+d1=dict({1:"Python", 2:"C++", 3:"Java", 4:"C"})
+d = d1
+print(id(d))
+print(id(d1))
+
+# d[2] ="C"
+# print(d)
+# print(d1) 
+# above is reference copy, both will change
+# d=d1.copy()
+# print(id(d))
+# print(id(d1))
+# d[2] ="C"
+# print(d)
+# print(d1) 
+
+#dictionary comprehension
+squares = {a:a*a for a in range(1,5)}
+print(squares)
---
+++ DataTypes/Functions.py
@@ -0,0 +1,312 @@
+'''
+Types of function
+- Predefined or built-in functions : which come installed along with python software
+  or built-in functions
+
+- User defined functions : which are defined by developers as per requirement 
+
+Terminology related to function in Python:
+
+def keyword:
+Every function should start with the keyword def.
+
+Name of the function:
+paranthesies
+colon:
+Body
+return statement is optional
+Syntax :
+def function_name():
+    code to be executed
+    return something(optional)
+'''
+
+# function without parameters
+# def person():
+#     print("My name is Bhauraj")
+#     print("I am from Bangalore")
+
+# person()
+# person()
+
+# function with parameters
+# def add(a,b):
+#     print(f"Sum of {a}+{b} : {a+b}")
+
+# def mult(a,b):
+#     print(f"Multiplication of {a}*{b} : {a*b}")
+
+# def sub(a,b):
+#     return a-b
+
+# add(20,10)
+# mult(20,10)
+
+# sub_result = sub(20,10)
+# print(sub_result)
+
+# Even or odd number 
+
+# def check_even_odd(num):
+#     if num%2 == 0:
+#         print(f"{num} is an even number")
+#     else:
+#         print(f"{num} is an odd number")
+
+# check_even_odd(20)
+# check_even_odd(30)
+
+# Return multiple values from the function
+'''
+A function that returns multiple values, same should be handled at the 
+function calling statement 
+
+'''
+# def function1(a,b):
+#     r1 = a+b
+#     r2 = a-b
+#     r3 = a*b
+#     r4 = a/b
+#     return r1,r2,r3,r4
+
+# result1,result2,result3,result4 = function1(20,10)
+# print(f"Addition: {result1}")
+# print(f"Substraction: {result2}")
+# print(f"Multiplication : {result3}")
+# print(f"Division: {result4}")
+
+# def get_person_details():
+#     name="Mahesh"
+#     age = 22
+#     salary = 42000000
+#     return name,age,salary
+
+# person_name, person_age, person_salary = get_person_details()
+
+# print(f"Person Name: {person_name} \n Person age: {person_age} \n Person salary: {person_salary}")
+
+# def calculate_statistics(numbers):
+#     minimum = min(numbers)
+#     maximum = max(numbers)
+#     total = sum(numbers)
+#     average = total/len(numbers)
+#     return minimum,maximum,total,average
+
+# result = calculate_statistics([5,10, 20, 30, 40, 50])
+# min_value,max_value, total_value, average_value = result
+# print(f"Minimum : {min_value}, maximum : {max_value}, total : {total_value}, average : {average_value}")
+
+# Function can call another function
+'''
+Syntax:
+def first_function():
+    code to be executed
+    def second_function():
+        code to be executed
+    we can call another function based on the requirements
+
+'''
+# def fun1():
+#     print("This is fun1")
+
+# def fun2():
+#     print("This is fun2")
+#     fun1()
+
+# fun2()
+
+# def multiply(a, b):
+#     return a*b
+
+# def add_and_multiply(a, b,x):
+#     addition_result = a+b
+#     multiplication_result = multiply(addition_result,x)
+#     return multiplication_result
+
+# result = add_and_multiply(10,20,30)
+
+# print(f"Result : {result}")
+
+# Function are first class objects
+'''
+Function are first class objects means they can be treated like any
+other objects such as integers, strings or lists
+- We can assign a function to variables
+- We can pass function as a parameter to another function
+- We can define one function inside another function
+- Even the function can return another function
+'''
+
+# def function1():
+#     print("This is a function1")
+
+# # Assign a function to a variable
+# res = function1
+
+# #call function
+# res()
+
+# def square(x):
+#     return x**2
+
+# def cube(x):
+#     return x**3
+
+# def apply_operation(fnc, x):
+#     return fnc(x)
+
+# result1 = apply_operation(square, 5)
+# result2 = apply_operation(cube, 3)
+
+# print(f"Square is {result1} and cube is {result2}")
+
+# Formal argument and Actual argument
+'''
+Formal arguments are variables that will hold the valued passed to the function
+Actual arguments are also known as arguments to the function which are values 
+passed to the function when it is called
+
+'''
+
+# Formal arguments
+# def welcome(name, age):
+#     print(f"Welcome {name}, you are {age} years old")
+
+# welcome("Sam", 45)# Actual arguments
+
+# Positional arguments
+'''
+Positional arguments are passed to the function based on their position or order
+If there are 3 formal arguments in function definition, then we need to send 3 actual arguments
+while calling the function
+'''
+
+# def fnc1(x,y):
+#     return x+y
+
+# res = fnc1(2,1)
+# print(f"Result : {res}")
+
+# Default arguments
+
+# def greet(name, age=35):
+#     print(f"Welcome {name}, you are {age} years old")
+
+# greet("Bhauraj")
+# greet("Sam",44)
+
+# def greet(name="Sam", age=35):
+#     print(f"Welcome {name}, you are {age} years old")
+
+# greet()
+# greet("Bhauraj")
+# greet("Bhauraj",37)
+
+# in the below example we cannot write only default values in the beginningdef greet(name="Sam", age=35):
+# def greet(name="Sam", age): # Error non default arguments should be there first
+#     print(f"Welcome {name}, you are {age} years old")
+
+# greet()
+# greet("Bhauraj")
+# greet("Bhauraj",37)
+
+# Keyword arguments
+'''
+Keyword arguments are passed to a function using the name of the parameter
+as a key-value pair.
+The order of arguments does not matter as long as they are specified by their
+parameter names
+'''
+
+# def cal_total(quantity, price):
+#     return quantity*price
+
+# total = cal_total(quantity=5, price=10)
+# total2= cal_total(price=15, quantity=4)
+
+# print(f"Total : {total}")
+# print(f"Total2 : {total2}")
+
+# def details(id, name):
+#     print(f"ID : {id} \n Name : {name}")
+
+# details(1001, name="Sam")
+
+# positinal arguments always should come first before keyword arguments, otherwise error comes
+# details(name="Kiran", 2000)
+
+# Variable length arguments
+'''
+Variable length arguments are used when we don't know how many arguments will be passed 
+to the function
+
+The variable length arguments is an argument that can accept any number of values, which
+is written with a * befor the variable in function definition
+
+Syntax:
+def function(*a):
+    code to be executed
+'''
+
+# def tot_cost(a, *b):
+#     sum = 0
+#     for i in b:
+#         sum += i
+#     print(a+sum)
+
+# tot_cost(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 21)
+# tot_cost(10, 20, 30)
+# tot_cost(10)
+# tot_cost(10, )
+
+# def total_cost(*args):
+#     sum = 0
+#     for i in args:
+#         sum += i
+#     return sum
+
+# result = total_cost(3,6,8,10,10,10)
+# print(result)
+
+# Keyword variable length arguments
+'''
+Just as variable length arguments, there are keywrd variable length arguments
+that are in key-value pairs
+
+Syntax:
+def function(** args):
+    code to be executed
+
+**args is keyword variable argument, so internally it represents a dictionary object
+which stores the data in the forms of Key-value pairs
+
+'''
+
+# def print_display(** kwrds):
+#     r=kwrds
+#     print(f"{r}{type(r)}")
+#     print(kwrds)
+#     for k,v in kwrds.items():
+#         print(f"key is {k} : value is {v}")
+
+# print_display(a=12, b=44, c=55)
+# print_display(empid=2121, empName="Suresh", empAge =55)
+
+# Global keyword
+'''
+Global keyword is used to indicate that a variable inside a function should refer to a
+global variable with the same name, rather than creating a new local variable within
+the function scope. This is necessary when we want to modify a global variable from within
+a function
+
+'''
+global_var = 10
+
+def modify_global():
+    global global_var
+    global_var = 20
+    print(f"Inside the function {global_var}")
+
+print(f"Before the function {global_var}")
+modify_global()
+print(f"After the function {global_var}")
